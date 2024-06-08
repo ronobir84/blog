6e5b06ddb2853e97735b6e1cf1cf228b07fe0b0b
ronodir84 <priashdas@84gmail.com>
2024-06-08 17:22:17 +0600

    add the all item and setup project

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbName = "blog_web";
+
+$config = new mysqli($host, $user, $password, $dbName);
+
+if (!$config) {
+    die("connection is undefined" . mysqli_connect_errno());
+} 
+
+else{
+    echo "Connection Success full";
+}
+
+
+
+?>
---
+++ partials/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+
+
+</head>
+
+<body>
+
+    <nav class="bg-white w-full h-[85px] border-gray-200 dark:bg-gray-900">
+        <div class="flex flex-wrap justify-between py-5 px-[70px]">
+            <div class="flex gap-14">
+                <a href="https://flowbite.com" class="flex items-center   ">
+                    <h1 class="text-4xl   font-bold   from-purple-400 via-pink-400 to-blue-400 bg-gradient-to-r bg-clip-text text-transparent uppercase relative bottom-1">Blog</h1>
+                </a>
+
+                <div>
+                    <ul class="flex gap-7">
+                        <li>
+                            <a class="" href="">
+                                <button class=" px-6 py-2 text-lg font-semibold text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:bg-gray-700">Home</button>
+                            </a>
+                        </li>
+
+                        <li>
+                            <div class="relative inline-block text-left">
+                                <div class="group">
+                                    <button type="button" class="inline-flex justify-center items-center w-full     font- text-lg  font-semibold  bg-gray-800 hover:bg-gray-700 focus:outline-none focus:bg-gray-700 px-5 py-2 text-white ">
+                                        Categories
+                                        <!-- Dropdown arrow -->
+                                        <svg class="w-6 h-6 ml-1 mt-[6px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M10 12l-5-5h10l-5 5z" />
+                                        </svg>
+                                    </button>
+
+                                    <!-- Dropdown menu -->
+                                    <div class="absolute left-0 w-[151px] h-28 mt-1 origin-top-left bg-white divide-y divide-gray-100 shadow opacity-0 invisible group-hover:opacity-100 group-hover:visible transition duration-700">
+                                        <div class="py-1">
+                                            <a href="#" class="block px-4 py-2 text-lg text-center font-semibold text-gray-700 hover:bg-gray-300">Politics</a>
+                                            <a href="#" class="block px-4 py-2 text-center text-lg font-semibold text-gray-700 hover:bg-gray-300">Sports</a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                        </li>
+
+
+                    </ul>
+                </div>
+
+
+            </div>
+
+
+
+            <div class="flex items-center space-x-6 rtl:space-x-reverse">
+                <div>
+                    <input class="bg-slate-50  0 font-mono  ring-zinc-400    outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-sm px-8 py-3 shadow-md focus:shadow-lg  border-2 border-sky-500   " placeholder="Search" name="text" type="search" />
+
+                </div>
+                <div>
+                    <div class=" relative right-[24.4px] bg-transparent items-center justify-center flex border-2 border-sky-500  hover:bg-sky-500 text-sky-500 hover:text-white duration-300 cursor-pointer active:scale-[0.98]">
+                        <button class="px-6 py-3"><a class="" href="">Search</a></button>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </nav>
